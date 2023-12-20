2d43d69a01f36eedaf585d37c47fd6e7722ab8c5
Akshata Jadahav <jakshata412@gmail.com>
2023-12-20 18:34:24 +0530

    variable to js added

---
+++ script.js
@@ -0,0 +1,57 @@
+// basic example
+
+//const message = "Hello, JavaScript!";
+//console.log(message);
+
+// example 2
+
+//const Früh = "foobar";
+//console.log(Früh);
+
+//declaration and initialization
+
+//let x;
+//console.log(x); 
+
+//variable scope
+
+//if (Math.random() > 0.5) {
+  //  const y = 5;
+  //}
+ // console.log(y); // ReferenceError: y is not defined
+
+ // Case-sensitive variable names
+
+//let firstName = "a";
+//let FirstName = "b";
+
+//console.log(firstName); // Outputs: "John"
+//console.log(FirstName); // Outputs: "Doe"
+
+//if (true) {
+//   var x = 5;
+// }
+//  console.log(x); // x is 5
+
+//variable hoisting
+
+//var x;
+//console.log(x === undefined); // true
+//x = 3;
+
+//(function () {
+// var x;
+//  console.log(x); // undefined
+//  x = "local value";
+// })();
+
+//constants
+
+//const MY_ARRAY = ["HTML", "CSS"];
+//MY_ARRAY.push("JAVASCRIPT");
+//console.log(MY_ARRAY); // ['HTML', 'CSS', 'JAVASCRIPT'];
+
+  
+  
+  
+
---
+++ variable.js
@@ -0,0 +1,15 @@
+// A variable is a container for storing data
+// A variable behaves as if it was the value that it contains
+
+// Two steps:
+// 1. Declaration(var,let,const)
+// 2. Assignment(= assignment operator)
+
+let firstName = "Akshata"; //strings
+let age = 21; //numbers
+let student = true; //booleans
+
+
+console.log("Hello", firstName);
+console.log("You are",age,"years old");
+console.log("Enrolled:", student);
