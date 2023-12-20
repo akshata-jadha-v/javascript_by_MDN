3e306b505e855732b2242710047e0e06e80d638d
Akshata Jadahav <jakshata412@gmail.com>
2023-12-20 19:16:43 +0530

    arithmetic expression to js added

---
+++ arithmeticexp.js
@@ -0,0 +1,30 @@
+// arithmetic expression is a combination of...
+// operands (values, variables, etc.)
+// operators (+ - * / %)
+// that can be evaluted to a value
+// eg. y = x + 5;
+
+let students = 20;
+
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//let extraStudents = students % 2;
+
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+
+/*
+   operator precedence
+   1. parenthesis ()
+   2. exponents
+   3. multiplication & division
+   4. addition & substraction
+*/
+let result = 1 + 2 * (3 + 4);
+
+console.log(result);
